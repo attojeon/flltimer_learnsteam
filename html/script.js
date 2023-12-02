f4730fdeba60d6afd28884bc936cbc518246af43
attojeon <atto.jeon@gmail.com>
2023-12-02 13:35:57 +0900

    경과시간 보여주기

--- html/script.js
+++ html/script.js
@@ -10,23 +10,35 @@ let interval = null;
 let isTimerRunning = false;
 
 function updateDisplay() {
-    const minutes = Math.floor(timer / 60).toString().padStart(2, '0');
-    const seconds = (timer % 60).toString().padStart(2, '0');
+    let minutes, seconds;
+    if (timer >= 0) {
+        // 정상적인 타이머 표시
+        minutes = Math.floor(timer / 60).toString().padStart(2, '0');
+        seconds = (timer % 60).toString().padStart(2, '0');
+    } else {
+        // 타이머가 0 이하일 때 경과 시간 표시
+        const elapsed = Math.abs(timer); // 음수 값을 양수로 변환
+        minutes = Math.floor(elapsed / 60).toString().padStart(2, '0');
+        seconds = (elapsed % 60).toString().padStart(2, '0');
+    }
     timerDisplay.textContent = `${minutes}:${seconds}`;
 }
 
 function startPauseTimer() {
     if (!isTimerRunning) {
-        startSound.play();
+        startSound.play(); // 0초가 되었을 때 사운드 재생
         interval = setInterval(() => {
             if (timer > 0) {
                 timer--;
-                updateDisplay();
-                if (timer === 30) {
-                    warningSound.play();
-                }
             } else {
-                clearInterval(interval);
+                timer--; // 0초에 도달한 후 계속 감소시켜 경과 시간을 표시
+            }
+            updateDisplay();
+
+            if (timer === 30) {
+                warningSound.play();
+            } else if (timer === 0) {
+                startSound.play(); // 0초가 되었을 때 사운드 재생
             }
         }, 1000);
         startPauseButton.textContent = 'PAUSE';
@@ -37,6 +49,35 @@ function startPauseTimer() {
         isTimerRunning = false;
     }
 }
+
+// function startPauseTimer() {
+//     if (!isTimerRunning) {
+//         startSound.play();
+//         interval = setInterval(() => {
+//             if (timer > 0) {
+//                 timer--;
+//                 updateDisplay();
+//                 if (timer === 30) {
+//                     warningSound.play();
+//                 }
+//             } else if (timer === 0) {
+//                 timer--;
+//                 updateDisplay();
+//                 startSound.play();
+//             }
+//             else {
+//                 clearInterval(interval);
+//                 console.log('Timer is already 0', timer);
+//             }
+//         }, 1000);
+//         startPauseButton.textContent = 'PAUSE';
+//         isTimerRunning = true;
+//     } else {
+//         clearInterval(interval);
+//         startPauseButton.textContent = 'START';
+//         isTimerRunning = false;
+//     }
+// }
 
 function resetTimer() {
     clearInterval(interval);
